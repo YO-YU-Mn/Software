b0dca8723d2b25fb48c956b53517f1b45d2f8b0f
youssef mohammed <ym884565@gmail.com>
2026-02-26 22:51:08 +0200

    install enveronment will use React Vite

---
+++ first_week/SoftWare/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Form from './components/Form'
+import './App.css'
+
+
+
+function App() {
+  
+return (
+    <>
+    <h1>Hello World!</h1>
+    <Form />
+    </>
+  )
+}
+
+export default App
