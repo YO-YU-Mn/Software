1501eb77c957a50f11fe09b3a16290e566c219c0
genidyabdelazeem-sketch <genidyabdelazeem@gmail.com>
2026-03-06 06:00:14 -0900

    Merge branch 'mostafa1_branch' of https://github.com/YO-YU-Mn/Software into ezzat_branch

---
+++ Schema/Admin.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "superadmin"],
+    default: "admin",
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
